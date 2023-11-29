28b32d94051c98253dfcc9753397ad6623e3cd0e
Matic, Andrija <matica@ae.com>
2023-11-29 02:39:43 +0100

    Create stage and prod clusters, and secret for use in prod cluster

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -80,6 +80,21 @@ network = google_compute_network.amatic-stage-vpc.self_link
 private_ip_google_access = true
 }
 
+resource "google_container_cluster" "amatic_stage_cluster" {
+  name     = "amatic-stage"
+  location = var.region
+  project  = var.project_id
+
+  node_pool {
+    name       = "stage-node-pool"
+    node_count = 1
+
+    node_config {
+      machine_type = "n1-standard-2"
+    }
+  }
+}
+
 #End of staging environment configuration
 
 #Production environment configuration
@@ -108,6 +123,65 @@ ip_cidr_range = var.prod_private_subnet_cidr_block
 region = var.region
 network = google_compute_network.amatic-prod-vpc.self_link
 private_ip_google_access = true
+}
+
+resource "google_container_cluster" "amatic_prod_cluster" {
+  name     = "amatic-prod"
+  location = var.region
+  project  = var.project_id
+
+  node_pool {
+    name       = "prod-node-pool"
+    initial_node_count = 3
+    autoscaling {
+      min_node_count = 3
+      max_node_count = 10
+    }
+
+    node_config {
+      machine_type = "n1-standard-4"  
+      disk_size_gb = 100              
+      preemptible  = false            
+    }
+  }
+
+  network_policy {
+    enabled = true 
+  }
+
+  logging_service = "logging.googleapis.com/kubernetes"
+  monitoring_service = "monitoring.googleapis.com/kubernetes"
+
+}
+resource "google_secret_manager_secret" "root_password_secret" {
+  secret_id = "root_password"
+  project = var.project_id
+
+  replication {
+    user_managed {
+      replicas {
+        location = var.region
+      }
+      replicas {
+        location = var.region
+      }
+    }
+  }
+}
+resource "google_secret_manager_secret" "db_password_secret" {
+  secret_id = "db_password"
+  project = var.project_id
+
+  replication {
+    user_managed {
+      replicas {
+        location = var.region
+      }
+      replicas {
+        location = var.region
+      }
+    }
+  }
 }
 
 #End of production environment configuration
